056de63d5c4aa4339956d8db7d7172f2469b4f48
Wesley <serpents123@gmail.com>
2022-12-07 09:23:15 -0800

    use nltk

--- backend/test.py
+++ backend/test.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import snscrape.modules.twitter as sntwitter
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
-query_array = [['html', 'html5'], 'markdown',
-               'css', 'SassCSS', 'getbootstrap', 'tailwindcss']
-
+query_array = [['html', 'html5']]
 
 def build_data(pass_query):
   snsquery = ''
@@ -17,7 +22,6 @@ def build_data(pass_query):
       else:
         snsquery = snsquery + " OR " + item
     snsquery = snsquery + ") " + lang
-    print(snsquery)
   else:
     query = pass_query
     snsquery = pass_query + " " + lang
@@ -35,22 +39,20 @@ def build_data(pass_query):
   tweets_df = pd.DataFrame(
       tweets, columns=['Date', 'Tweet', 'User', 'Tweet ID', 'Tweet Url'])
 
-  # Remove @, #, RT, links, and new line escape character. Done using regex substitution.
-  # These do not add anything to polarity and sentiment and could confuse the algorithm.
+  def cleanTweets(tweet):
+    tweet = re.sub('@[A-Za-z]+', '', tweet)
+    tweet = re.sub('#', '', tweet)
+    tweet = re.sub('RT[\s]+', '', tweet)
+    tweet = re.sub('https?:\/\/\S+', '', tweet)
+    tweet = re.sub('\n', ' ', tweet)
+    tweet = tweet.lower()
+    tweet = tweet.split()
+    wl = WordNetLemmatizer()
+    tweet = [wl.lemmatize(word) for word in tweet if not word in set(stopwords.words('english'))]
+    tweet = ' '.join(tweet)
+    return tweet
 
-  # def cleanTweets(tweet):
-  #   tweet = re.sub('@[A-Za-z0-9_]+', '', tweet)
-  #   tweet = re.sub('#', '', tweet)
-  #   tweet = re.sub('RT[\s]+', '', tweet)
-  #   tweet = re.sub('https?:\/\/\S+', '', tweet)
-  #   tweet = re.sub('\n', ' ', tweet)
-  #   return tweet
-
-  # Apply cleanTweets to every single item in the tweets column
-
-  # tweets_df['Cleaned Tweets'] = tweets_df['Tweets'].apply(cleanTweets)
-
-  # Save this dataframe
+  tweets_df['Cleaned Tweet'] = tweets_df['Tweet'].apply(cleanTweets)
 
   tweets_df.to_csv(f"{query}.csv", index=False)
 
