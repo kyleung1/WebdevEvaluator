0e0c7d643e2f134110b6f47e7e30e4bc5abf088c
Kyle <leungkyle2@gmail.com>
2022-12-15 10:45:52 -0800

    asdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import Image from 'next/image'
+"use client";
+import Link from "next/link";
 
 export default function Home() {
+
+  function searchBtn() {
+
+  }
+
   return (
     <div>
       <main>
@@ -8,9 +14,10 @@ export default function Home() {
         <p className='text-xl text-center mt-52'>Enter a Language, Database, Cloud.</p>
         <div className='flex items-stretch justify-center'>
           <input className="border-2 border-indigo-500 rounded-md m-0.5" type="text"/>
-          <button className="border-2 border-indigo-500 rounded-md p-1 m-0.5">Search</button>
+          <Link href="/result">
+            <button className="border-2 border-indigo-500 rounded-md p-1 m-0.5">Search</button>
+          </Link>
         </div>
-
       </main>
     </div>
   )
