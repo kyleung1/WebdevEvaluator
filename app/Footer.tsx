aee778f6093df9a8a77c558b83eeea6eabd051b7
Wesley <serpents123@gmail.com>
2022-12-20 14:26:43 -0800

    fix error

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Footer = () => {
   return (
     <div className='bg-indigo-500 p-5'>
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const loading = () => {
   return (
     <div>loading</div>
