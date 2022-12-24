aa338569ea38ec364077b41e862e59af774ff5c1
Kyle <leungkyle2@gmail.com>
2022-12-23 23:45:49 -0800

    comments

--- app/result/[tech]/page.tsx
+++ app/result/[tech]/page.tsx
@@ -9,31 +9,12 @@ type PageProps = {
 };
 
 async function getCsv() {
-    // // const parser = createParser();
-    // fs.createReadStream("../../../backend/Other/Git.csv")
-    // const csv = await fetch("/../../../backend/Other/Git.csv");
-    // const text = await csv.text();
-    // return text;
-    // const records = [];
-
     const parser = parse({columns: true}, function (err, records) {
         console.log(records);
     })
     // C:/Users/chky/Desktop/js/WebdevEvaluator/backend/Other/Git.csv works for aboslute paths
     fs.createReadStream("../../../../backend/Other/Git.csv").pipe(parser)
-    // const parser = parse({
-    //     delimiter: ","
-    // });
-    // parser.on("readable", function(){
-    //     let record;
-    //     while ((record = parser.read()) !== null) {
-    //         records.push(record);
-    //     }
-    // });
-    // parser.on("error", function(err){
-    //     console.error(err.message);
-    // });
-    // parser.on
+
     return "hi";
 }
 
