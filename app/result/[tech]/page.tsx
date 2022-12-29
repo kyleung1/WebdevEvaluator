ecfc2672631e741d0aeaabb62852c82ff90a0343
Kyle <leungkyle2@gmail.com>
2022-12-28 23:55:49 -0800

    fixed barchart

--- app/result/[tech]/page.tsx
+++ app/result/[tech]/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 //https://github.com/swimlane/ngx-charts/issues/1686 for reference
 import * as d3 from 'd3';
-import { useRef, useEffect } from 'react';
-
+import { useEffect } from 'react';
 type PageProps = {
     params: {
         tech: string;
@@ -40,15 +39,25 @@ const results = ({params: {tech}}: PageProps) => {
             let twentyWords: Array<[string, number]> = countsList.slice(0,20);
             return twentyWords;
         }
+
+        function pieChart() {
+            //making a pie chart
+        }
         
         function barGraph(data: Array<[string, number]>) {
+            const title = document.createElement("h2");
+            title.textContent = "Word Count in Tweets Containing: " + techSplit[1];
+            title.classList.add("text-indigo-500");
+            title.classList.add("text-xl");
+            title.classList.add("text-center");
+            document.getElementById("bar")?.appendChild(title);
+
             // Set the dimensions of the graph
             const width = 500;
             const height = 300;
-            const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+            const margin = { top: 20, right: 20, bottom: 80, left: 40 };
         
-            const x = d3
-            .scaleBand()
+            const x = d3.scaleBand()
             .rangeRound([0, width])
             .padding(0.1);
         
@@ -57,23 +66,21 @@ const results = ({params: {tech}}: PageProps) => {
             x.domain(data.map(d => d[0]));
             y.domain([data[19][1], data[0][1]]);
         
-            const svg = d3
-                .select('#chart')
+            const svg = d3.select('#bar')
                 .append('svg')
                 .attr('width', width + margin.left + margin.right)
                 .attr('height', height + margin.top + margin.bottom)
                 .append('g')
                 .attr('transform', `translate(${margin.left},${margin.top})`);
         
-            svg
-                .append('g')
+            svg.append('g')
                 .attr('class', 'axis axis--x')
                 .attr('transform', `translate(0,${height})`)
                 .call(d3.axisBottom(x));
             
             svg.append('g').attr('class', 'axis axis--y').call(d3.axisLeft(y));
-            svg
-              .selectAll('.bar')
+
+            svg.selectAll('.bar')
               .data(data)
               .enter()
               .append('rect')
@@ -82,23 +89,27 @@ const results = ({params: {tech}}: PageProps) => {
               .attr('y', d => y(d[1]))
               .attr('width', x.bandwidth())
               .attr('height', d => height - y(d[1]));
+
+            svg.selectAll(".axis--x text")
+              .style("text-anchor", "end")
+              .attr("dx", "-.8em")
+              .attr("dy", ".15em")
+              .attr("transform", "rotate(-65)");
         }
 
-        async function b () {
+        async function init () {
             const sentiments = await getCsv(techSplit[0]);
-            const a = await get20Words(techSplit[0]);
-            barGraph(a);
+            const twentyWords = await get20Words(techSplit[0]);
+            barGraph(twentyWords);
         }
         
-        b();
+        init();
     })
-
-    
-    // barGraph(a);
     return (
         <div className="flex flex-col items-center">
             <h1 className="text-3xl">{techSplit[1]}</h1>
-            <div id="chart"></div>
+            <div id="bar"></div>
+            <div id="pie"></div>
         </div>
     )
 }
---
+++ app/result/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Nothing selected.</div>
+  )
+}
+
+export default page
