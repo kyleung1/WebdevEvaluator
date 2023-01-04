db10ff6a34799c81893690f139ab28e148013d55
Wesley <serpents123@gmail.com>
2023-01-03 16:44:08 -0800

    configure gradle

---
+++ backend/webdeveval/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.0.1"
+	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+}
+
+group = "com.webdeveval"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.jsoup:jsoup:1.15.3")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.mongodb:mongodb-driver-sync:4.8.1")
+	implementation ("com.google.code.gson:gson:2.10")
+	implementation("io.github.cdimascio:dotenv-kotlin:6.4.0")
+	implementation("net.sourceforge.htmlunit:htmlunit:2.37.0")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
